20e7f6c5f38020aebb89f60a011310f3f59f0f9a
Hanna Xu <hello@hannaxu.com>
2016-04-17 17:17:10 -0700

    Added Toolbar

---
+++ BlocBrowser/BlocBrowser/AwesomeFloatingToolbar.h
@@ -0,0 +1,29 @@
+//
+//  AwesomeFloatingToolbar.h
+//  BlocBrowser
+//
+//  Created by Hanna Xu on 4/17/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AwesomeFloatingToolbar;
+
+@protocol AwesomeFloatingToolbarDelegate <NSObject>
+
+@optional
+
+- (void) floatingToolbar:(AwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title;
+
+@end
+
+@interface AwesomeFloatingToolbar : UIView
+
+- (instancetype) initWithFourTitles: (NSArray *)title;
+
+- (void) setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title;
+
+@property (nonatomic, weak) id <AwesomeFloatingToolbarDelegate> delegate;
+
+@end
